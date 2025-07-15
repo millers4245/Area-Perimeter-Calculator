e1097e2ead6640b52de690cde8be821dece69d58
millers4245 <millers4245@masseyhigh.school.nz>
2025-07-16 11:48:28 +1200

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 print ("hello world")
+
+# print(greeting)
+
+print ("fav_num")
+fav_num=7
+num_string = "5"
+print(fav_num * 2)
+print(num_string * 5)
+
+love_programming = True
+
+PI = 3.14
+a_01_my_num = 7
