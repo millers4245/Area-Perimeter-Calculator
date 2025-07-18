d648b28d8934c827afdbe24e0ecd8d93f8593cf8
millers4245 <millers4245@masseyhigh.school.nz>
2025-07-18 14:22:34 +1200

    Add files via upload

---
+++ input and output.py
@@ -0,0 +1,16 @@
+
+username = input("whats your name? ")
+
+fav_num = int(input("What is your favourite number?  "))
+
+double = fav_num * 2
+halve = fav_num / 2
+square = fav_num * fav_num
+
+print(f"\nHi {username}, your favourite number is {fav_num}")
+
+print(f"Double {fav_num} is {double}.")
+print(f"Half {fav_num} is {halve}")
+print(f"{fav_num} squared is {square}")
+print()
+print("Have a nice day.")
