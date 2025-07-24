ca320a9d72f7373953d16b5f3429987a302ab86b
millers4245 <millers4245@masseyhigh.school.nz>
2025-07-24 12:24:34 +1200

    Add files via upload

---
+++ area perimeter calc.py
@@ -0,0 +1,10 @@
+
+width = float(input("Width:  "))
+height = float(input("Height:  "))
+
+area = width * height
+perimeter = 2 * (width + height)
+
+print()
+print(f"Perimeter: {perimeter} units")
+print(f"Area: {area} square units")
